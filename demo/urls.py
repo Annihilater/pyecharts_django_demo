5616c64fde6ed9fa1d7f64737a2afd2555b2caab
Annihilater <yanmiexingkong@gmail.com>
2020-04-17 18:11:38 +0800

    feat: write views function

---
+++ demo/urls.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2020/4/17 6:01 下午
+# @Author: yanmiexingkong
+# @email : yanmiexingkong@gmail.com
+# @File  : urls.py
+
+
--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from jinja2 import Environment, FileSystemLoader
+from pyecharts.globals import CurrentConfig
+from django.http import HttpResponse
+
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+
+CurrentConfig.GLOBAL_ENV = Environment(loader=FileSystemLoader("./demo/templates"))
+
+
+def index(request):
+    c = (
+        Bar()
+            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+            .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
+            .add_yaxis("商家B", [15, 25, 16, 55, 48, 8])
+            .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
+    )
+    return HttpResponse(c.render_embed())
